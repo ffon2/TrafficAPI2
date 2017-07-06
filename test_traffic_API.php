ee83f929bbeb749e8a3660f38a649d1c463552e3
ffon2 <pikul.k@ku.th>
2017-07-06 16:15:29 +0700

    Update test_traffic_API.php

--- test_traffic_API.php
+++ test_traffic_API.php
@@ -1,6 +1,6 @@
 <?php
 $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting";
-$strAccessToken = true;
+$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 $post_data =
             $date_time="2017-04-26T08:00:00".
             $st_y="13.8036917".
