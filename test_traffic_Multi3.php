101e5e3a030f4248ed6e9302f2cbbe658a5145d7
ffon2 <pikul.k@ku.th>
2017-07-07 11:26:56 +0700

    Add files via upload

---
+++ test_traffic_Multi3.php
@@ -0,0 +1,25 @@
+<?php
+$stops = $_POST['stops'];
+$vehicles = $_POS['vehicles'];
+$commons = $_POS['commons'];
+
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/mvrp";
+$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";	
+
+$header = array(
+'Content-Type:application/x-www-form-urlencoded',
+'Authorization: Bearer ' . $strAccessToken
+);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POS, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_POS);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+$result = curl_exec($ch);
+curl_close($ch);
+echo "OK";
+var_dump($result)."<br>";
+
