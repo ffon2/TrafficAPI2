2b704902c9050d1d2eeb75a7fcc575fbe37e4dc7
ffon2 <pikul.k@ku.th>
2017-07-07 11:06:11 +0700

    Add files via upload

---
+++ test_traffic_API_Multi.php
@@ -0,0 +1,30 @@
+<?php
+$date_time = $_GET['date_time'];
+$polyline = $_GET['polyline'];
+$service_time = $_GET['service_time'];
+$mode = $_GET["mode"];
+$toll = $_GET['toll'];
+$vehicle = $_GET['vehicle'];
+
+$url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/smvrp";
+$strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
+//$data = 'date_time=2017-04-26T08:00:00&st_y=13.8036917&st_x=100.5542416&en_y=13.8755595&en_x=100.6191486&toll=1'; 
+	
+
+$header = array(
+'Content-Type: application/json',
+'Authorization: Bearer ' . $strAccessToken
+);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_GET, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $_GET);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+$result = curl_exec($ch);
+curl_close($ch);
+echo "OK";
+var_dump($result)."<br>";
+
