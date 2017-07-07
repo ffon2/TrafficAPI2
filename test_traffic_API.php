f85dee3c4150f570253033856ebc76eb03e4f7cf
ffon2 <pikul.k@ku.th>
2017-07-07 11:11:07 +0700

    Update test_traffic_API.php

--- test_traffic_API.php
+++ test_traffic_API.php
@@ -4,7 +4,7 @@ $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
 $data = 'date_time=2017-04-26T08:00:00&st_y=13.8036917&st_x=100.5542416&en_y=13.8755595&en_x=100.6191486&toll=1'; 
 
 $header = array(
-//'Content-Type: application/json',
+'Content-Type: application/json',
 'Authorization: Bearer ' . $strAccessToken
 );
 
