2efb2fff2a9b536cfce3bd47e59f4d675768d9cf
ffon2 <pikul.k@ku.th>
2017-07-07 10:43:51 +0700

    Update test_traffic_API.php

--- test_traffic_API.php
+++ test_traffic_API.php
@@ -1,10 +1,12 @@
 <?php
+<?php
 
 $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/odRouting";
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";
+$data = 'date_time=2017-04-26T08:00:00&st_y=13.8036917&st_x=100.5542416&en_y=13.8755595&en_x=100.6191486&toll=1'; 
 
 $header = array(
-'Content-Type: application/json',
+//'Content-Type: application/json',
 'Authorization: Bearer ' . $strAccessToken
 );
 
@@ -13,7 +15,7 @@ curl_setopt($ch, CURLOPT_GET, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $_GET);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 $result = curl_exec($ch);
 curl_close($ch);
@@ -22,3 +24,7 @@ var_dump($result)."<br>";
 //var_dump($ch)."<br>";
 var_dump($post_data)."<br>";
 var_dump($_GET)."<br>";
+
+
+
+
