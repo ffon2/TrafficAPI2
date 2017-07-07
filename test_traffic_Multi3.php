c426670edc4daef66b775622c82fad64e81ff5a4
ffon2 <pikul.k@ku.th>
2017-07-07 12:18:51 +0700

    Update test_traffic_Multi3.php

--- test_traffic_Multi3.php
+++ test_traffic_Multi3.php
@@ -1,7 +1,7 @@
 <?php
-$stops = array($_POST['stops']);
-$vehicles = array($_POS['vehicles']);
-$commons = $_POS['commons'];
+$stops =    $_POST['stops'];
+$vehicles = $_POS['vehicles'];
+$commons =  $_POS['commons'];
 
 $url = "https://apiplatform.rtic-thai.info:8243/logistics/1.0.0/mvrp";
 $strAccessToken = "31cc9ed0-50f1-3b99-b4d1-8aca20de6213";	
